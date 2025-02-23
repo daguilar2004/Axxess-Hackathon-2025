acea2099e2ae189ba5e9c7928db1d4dad09a3593
daguilar2004 <118065951+daguilar2004@users.noreply.github.com>
2025-02-23 02:06:44 -0600

    Initialize Auth0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import  './index.css'
 import router from './router/index.js';  // Ensure you are importing the router
+import  './index.css'
 
 
-// import { createAuth0 } from '@auth0/auth0-vue';
+
+import { createAuth0 } from '@auth0/auth0-vue';
 const app = createApp(App);
 
-// app.use(
-//   createAuth0({
-//     domain: "dev-qug84gqbi343u542.us.auth0.com",
-//     clientId: "nEl4l6ANYdXN5yWrlz51S6tsv4UWwkZk",
-//     authorizationParams: {
-//       redirect_uri: window.location.origin,
-//     }
-//   })
-// );
+app.use(
+  createAuth0({
+    domain: "dev-qug84gqbi343u542.us.auth0.com",
+    clientId: "pfjIj0QDI640Q7M2sE2bL8hwLiY4lnNF",
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    }
+  })
+);
 app.use(router);
 app.mount('#app');
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import LoginPage from '../components/LoginPage.vue'; 
+import LogoutPage from '../components/LogoutPage.vue'; 
 import SignUpPage from '../components/SignupPage.vue';
 import LearnMorePage from '../components/LearnMorePage.vue';
 import ChatBox from '../views/ChatBox.vue';
 //import ChatBox from '../views/ChatTest.vue';
+
 const routes = [
 
   { path: '/', component: HomePage },
   { path: '/login', component: LoginPage },
+  { path: '/logout', component: LogoutPage },
   { path: '/signup', component: SignUpPage },
   { path: '/learn-more', component: LearnMorePage },
   { path: '/chatBox', component: ChatBox }
-
   
 ];
 
